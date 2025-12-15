609695c600456ff62b4864bada00d382992521c0
raf <rdelos747@gmail.com>
2025-12-15 17:29:40 -0300

    save 2025-12-15

--- startup.sh
+++ startup.sh
@@ -3,4 +3,10 @@ function start() {
   startx ~/.xinitrc $1
 }
 
+# Force alacritty to recover color scheme
+if command -v wal > /dev/null 2>&1 && [ "$TERM" = "xterm-kitty" ]; then
+    # Run 'wal' command with options to recover color scheme, ignore output, and ignore updating other apps
+    wal -Rqe
+fi
+
 PS1="\033[37;42m\w\033[0m \$(get_git_branch_ps1)"
